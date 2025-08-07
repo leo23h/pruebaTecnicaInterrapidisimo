fec4f8dc7c91721cea059e75b6cd9583fa8b9fd1
leo23h <leo.23h1991@gmail.com>
2025-08-06 22:43:47 -0500

    creacion de componentes a utilizar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MateriaComponent } from './materia/materia.component';
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { EstudianteDetalleComponent } from './estudiante-detalle/estudiante-detalle.component';
 
 export const routes: Routes = [
   { path: 'auth', component: SignInComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'subject', component: MateriaComponent },
+  {
+    path: 'student',
+    component: EstudianteComponent,
+    children: [
+      { path: 'student-detail/:id', component: EstudianteDetalleComponent },
+    ],
+  },
   { path: '**', redirectTo: '' },
 ];
